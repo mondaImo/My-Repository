73264b5225d69517766c28008fac5ffa534756c3
mondaImo <mondald107@gamil.com>
2024-02-24 14:14:23 +0530

    add code to dev

--- directory2/HelloWorld.java
+++ directory2/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Made changes in code");
+		int a = 10, b = 15 ;
+		System.out.println("This will print summation : " + (a+b));
 	}
 }
